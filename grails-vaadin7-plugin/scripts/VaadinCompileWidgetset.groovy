e59ac9daea6065c6a5828f89211c73edc999af64
Christoph Frick <rid@ofnir.net>
2014-05-05 10:08:50 +0200

    rename the script from VaadinCompile to VaadinCompileWidgetset; add a short descripton to the task

--- grails-vaadin7-plugin/scripts/VaadinCompileWidgetset.groovy
+++ grails-vaadin7-plugin/scripts/VaadinCompileWidgetset.groovy
@@ -11,7 +11,11 @@ target(widgetset_init: "init") {
     ant.property(name: "generate.widgetset", value: "1")
 }
 
-target(compile_widgetset: "widgetset") {
+USAGE = """
+vaadin-compile-widgetset
+Compiles the widgetset configured in grails-app/conf/VaadinConfig.groovy
+"""
+target(compile_widgetset: "Compiles the widgetset configured in grails-app/conf/VaadinConfig.groovy") {
     depends(classpath, compile, widgetset_init)
     ant.echo message: """Compiling ${ant.project.properties.'widgetset'} into ${ant.project.properties."client-side-destination"} directory..."""
     ant.java(classname: "com.google.gwt.dev.Compiler", maxmemory: "512m", failonerror: true, fork: true, classpathref: "grails.compile.classpath") {
